28c695660f80fa76901f42e797c1ce03a407418d
Рамиль Ракаев <CHICO5Listo4@gmail.com>
2021-10-25 15:46:54 +0300

    Добавлен WebhookController

---
+++ TelegramWebApp/Controllers/WebhookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using TelegramBotBusiness;
+
+namespace TelegramWebApp.Controllers
+{
+    public class WebhookController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Post(
+            [FromBody] Update update)
+        {
+            await TelegramBot.EchoAsync(update);
+            return Ok();
+        }
+    }
+}
